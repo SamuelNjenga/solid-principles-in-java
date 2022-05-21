84d74949657bb23cb47a71a29cd8f19da6137c5a
Samuel Njenga <snjenga704@gmail.com>
2022-05-21 20:27:21 +0300

    feat: initial commit

---
+++ src/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    private final int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
